6db1784d892ffd1a7e52ad2a9c6fa09b999de2b7
Arun Joy Thekkiniyath <16219238+arunjoyt@users.noreply.github.com>
2024-04-26 14:09:19 +0530

    Update hooks.py

--- lhs/hooks.py
+++ lhs/hooks.py
@@ -2,7 +2,7 @@ app_name = "lhs"
 app_title = "LHS"
 app_publisher = "Arun Joy Thekkiniyath"
 app_description = "Learning Helper System"
-app_email = ""
+app_email = "contact@lhs.com"
 app_license = "mit"
 # required_apps = []
 
