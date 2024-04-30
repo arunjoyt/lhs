99c9aaee506461d50907d8a445291a98a1998abf
Arun Joy Thekkiniyath <16219238+arunjoyt@users.noreply.github.com>
2024-04-30 09:16:27 +0000

    added random selection of 10 questions

--- lhs/www/translate_to_german.py
+++ lhs/www/translate_to_german.py
@@ -1,9 +1,15 @@
 import frappe
+import random
 
 def get_context(context):
     master_records = frappe.get_all("LHS Translation Master")
-    context.translations = []
+    translations = []
     for record in master_records:
         master_doc = frappe.get_doc("LHS Translation Master", record.name)
-        context.translations.extend(master_doc.translations)
+        translations.extend(master_doc.translations)
+    # select a random sample of 10 translations
+    if len(translations) > 10: 
+        context.translations = random.sample(translations, 10)
+    else:
+        context.translations = translations
     return context
