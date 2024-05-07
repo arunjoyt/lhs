d1326e37f69c22b25dd8b7a9bf45f12c230e14c1
Arun Joy Thekkiniyath <16219238+arunjoyt@users.noreply.github.com>
2024-05-07 18:00:38 +0530

    removed old table

--- lhs/lhs/doctype/lhsuserscore/lhsuserscore.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Arun Joy Thekkiniyath and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class LHSUserScore(Document):
-	pass
