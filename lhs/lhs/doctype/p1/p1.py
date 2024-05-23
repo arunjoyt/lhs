05bfd14afceed99a3c7d3146bb8683ed3d4c759d
Arun Joy Thekkiniyath <16219238+arunjoyt@users.noreply.github.com>
2024-05-23 12:32:28 +0530

    deleted unused doctypes

--- lhs/lhs/doctype/p1/p1.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Arun Joy Thekkiniyath and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.website.website_generator import WebsiteGenerator
-
-
-class P1(WebsiteGenerator):
-	pass
